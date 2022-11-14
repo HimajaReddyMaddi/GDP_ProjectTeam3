8fd1aaf582fdcfd11e0d75ba97297d900251c8b7
HimajaReddyMaddi <89427685+HimajaReddyMaddi@users.noreply.github.com>
2022-11-13 22:10:54 -0600

    Update ChatMessageController
    added code for displaying the chat messages in the addChatMessage html page

--- src/main/java/com/farmatrix/demo/controller/ChatMessageController.java
+++ src/main/java/com/farmatrix/demo/controller/ChatMessageController.java
@@ -30,7 +30,49 @@ public class ChatMessageController {
 		
 		model.addAttribute("chatmessage",chatMessage);
 		return "addchatmessage";
+		
+		List<ChatMessage> chatMessages = service.findAllInbox((String) request.getSession().getAttribute("username")); // model.addAttribute("chatmessages",chatMessages);
+		System.out.println("In user " + request.getSession().getAttribute("username"));
+		List<ChatMessage> chatMessages1 = service.findAllInbox("admin@admin.com");
+		List<String> message = new ArrayList<>();
+		System.out.println("In 1at method -------------");
+		for (ChatMessage bean : chatMessages) {
+			System.out.println("In 1at method for1-------------");
+			message.add(bean.getMessage());
+			System.out.println(message);
+		}
+		// to fetch the messages from admin
+		List<String> message1 = new ArrayList<>();
+		for (ChatMessage bean : chatMessages1) {
+				message1.add(bean.getMessage());
+
+		}
+
+		
+		  List<String> sender1 = new ArrayList<>(); 
+		  for (ChatMessage bean :chatMessages) { 
+			  System.out.println("In 1at method for3 if-------------");
+			  sender1.add(bean.getSender()); 
+			  System.out.println(sender1);
+			  }
+		  List<String> messageFinal = new ArrayList<>();
+		
+		  for (int i = 0, j = 0; i < message.size() && j < message1.size(); i++, j++) {
+		  System.out.println("In for ");
+		  if(request.getSession().getAttribute("username").equals("admin@admin.com") ) {
+		  messageFinal.add(sender1.get(j) + " : " + message.get(j));
+		  System.out.println("In if " + request.getSession().getAttribute("username"));
+		  } else
+		  { 
+			  System.out.println("In 1at method for else-------------");
+			  messageFinal.add(request.getSession().getAttribute("username") + " : " +message1.get(j)); // 
+				 messageFinal.add("admin : " +message.get(i));
+		  }
+		  }
+		model.addAttribute("messageFinal", messageFinal);
+		return "addchatmessage";
 	}
+	
 
 	@RequestMapping(value = "/savechatmessage",method = RequestMethod.POST)
 	public String saveChatMessage(HttpServletRequest request, Model model,@ModelAttribute("chatmessage") ChatMessage chatMessage)
